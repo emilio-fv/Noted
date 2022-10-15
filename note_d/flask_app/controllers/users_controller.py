2c7d8cd96f77affebe9fb513f6cc94d63d887816
emilio-fv <emiliofv.sd@gmail.com>
2022-10-15 14:20:01 -0700

    Added comments to routes

--- note_d/flask_app/controllers/users_controller.py
+++ note_d/flask_app/controllers/users_controller.py
@@ -11,58 +11,58 @@ import Constants
 # Initialize Bcrypt object
 bcrypt = Bcrypt(app) 
 
-# ========== Spotify API ==========
+# ==== Spotify API ====
 CLIENT_ID = Constants.CLIENT_ID
 CLIENT_SECRET = Constants.CLIENT_SECRET
-sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID,
-                                                           client_secret=CLIENT_SECRET))
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
 
-# ========== ROUTES ==========
+# ==== Login & Registration ====
 # TODO: Route for splash page
 
-@app.route('/') # Route to render login/register page
+@app.route('/') # ROUTE: login/register page
 def index():
+    # TODO: Check if usre is logged in
     return render_template('login_register.html')
 
-@app.route('/register', methods=['POST']) # Route to process register form data
+@app.route('/register', methods=['POST']) # ROUTE: register user
 def register():
-    if not User.validate(request.form): # Validate user input
+    if not User.validate(request.form): # Validate form data
         return redirect('/')
     hashed_password = bcrypt.generate_password_hash(request.form['password']) # Hash password
     data = {
         **request.form,
         'password': hashed_password
     }
-    id = User.create(data) # Create user (SQL Query returns id)
+    id = User.create(data) # Add user to database
     session['user_id'] = id # Initialize session
-    return redirect('/dashboard') # Redirect 
+    return redirect('/dashboard') # Redirect to dashboard
 
-@app.route('/users/login', methods=['POST']) # Route to login
+@app.route('/users/login', methods=['POST']) # ROUTE: login user
 def login():
     user_in_db = User.get_one_by_email({'email': request.form['email']}) # Validate email
     if not user_in_db:
         flash('Invalid login info', "log")
         return redirect('/')
-    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']):
+    if not bcrypt.check_password_hash(user_in_db.password, request.form['password']): # Validate password
         flash("Invalid login info", "log")
         return redirect('/')
-    session['user_id'] = user_in_db.id
-    return redirect('/dashboard')
+    session['user_id'] = user_in_db.id # Initialize session
+    return redirect('/dashboard') # Redirect: Dashboard
 
-@app.route('/users/logout') # Route to logout
+@app.route('/users/logout') # ROUTE: logout user
 def logout():
-    session.clear()
-    return redirect('/')
+    session.clear() # Clear session
+    return redirect('/') # Redirect to register/login page
 
-@app.route('/dashboard') # Route to dashboard
+# ==== Dashboard ====
+@app.route('/dashboard') # ROUTE: dashboard
 def dashboard():
-    if 'user_id' not in session: # Check if user logged in
+    if 'user_id' not in session: # Check if user not logged in
         return redirect('/')
-    logged_user = User.get_one_by_id({'id': session['user_id']})
-    logged_user_reviews = Review.get_all_by_user_id({'user_id': session['user_id']})
+    logged_user = User.get_one_by_id({'id': session['user_id']}) # Get user's data
+    logged_user_reviews = Review.get_all_by_user_id({'user_id': session['user_id']}) # Get user's reviews
     for review in logged_user_reviews:
-        # Get album data
-        album_results = sp.album(review.album_id)
+        album_results = sp.album(review.album_id) # Get album data for each review
         album_data = {
             "album_id": album_results['id'],
             "album_name": album_results['name'],
@@ -71,15 +71,13 @@ def dashboard():
         review.album_data = album_data
     return render_template('dashboard.html', logged_user = logged_user, logged_user_reviews = logged_user_reviews)
 
-# ========== Music Search Tab ==========
-@app.route('/users/music/search_form') # Route to search music page
+# ==== Music Tab ====
+@app.route('/users/music/search_form') # ROUTE: search music engine
 def search_music_form(all_albums=[]):
     return render_template('music_search.html') 
 
-@app.route('/users/music/search', methods=['POST'])
+@app.route('/users/music/search', methods=['POST']) # ROUTE: search for music
 def search_music():
-    # print(request.form) # Print form data
-    # Process music search and return list of search results
     album_name = request.form['album_name']
     search_results = sp.search(album_name, limit=4, offset=0, type='album', market=None)
     album_results = search_results['albums']['items']
@@ -93,7 +91,7 @@ def search_music():
         all_albums.append(one_album)
     return jsonify(all_albums)
 
-@app.route('/users/music/view/<album_id>') # Route to view an album's info 
+@app.route('/users/music/view/<album_id>') # ROUTE: view album info 
 def view_music(album_id):
     album_results = sp.album(album_id)
     album_data = {
@@ -103,12 +101,12 @@ def view_music(album_id):
     }
     return render_template('music_view.html', album_data = album_data)
 
-# ========== Users Search Tab ==========
-@app.route('/users/user_search') # Route to search users page
+# ==== Users Tab ====
+@app.route('/users/user_search') # ROUTE: search users page
 def search_users():
     return render_template('user_search.html')
 
-@app.route('/users/user_view') # Route to view a user's profile
+@app.route('/users/user_view') # ROUTE: view user's profile
 def view_user():
     return render_template('user_view.html')
 
