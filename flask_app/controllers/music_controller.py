c03faacf27f9c02842484d01f15d0c3bf2854f1a
emilio-fv <emiliofv.sd@gmail.com>
2022-12-08 21:03:00 -0700

    Update view album

--- flask_app/controllers/music_controller.py
+++ flask_app/controllers/music_controller.py
@@ -1,6 +1,7 @@
 from flask_app import app
 from flask import redirect, render_template, request, jsonify
 import flask_app.constants
+from flask_app.models.review_model import Review
 from flask_app.controllers.helpers import login_required, sp
 
 @app.route('/music/search') # Search Music Form
@@ -65,6 +66,29 @@ def view_album(album_id):
         "album_img": album_results['images'][0]['url'],
         "album_tracks": album_tracks
     }
-    return render_template('album_view.html', album_data = album_data)
+    reviews = Review.get_all_by_album_id({'album_id': album_id})
+    all_reviews = []
+    average_rating = 0
+    for review in reviews:
+        average_rating += review.rating
+        this_review = {
+            'id': review.id,
+            'album_name': review.album_name,
+            'artist_name': review.artist_name,
+            'img_url': review.img_url,
+            'date': review.date,
+            'rating': review.rating,
+            'text': review.text,
+            'created_at': review.created_at,
+            'updated_at': review.updated_at,
+            'user_id': review.user_id,
+            'username': review.user.username,
+            'first_name': review.user.first_name,
+            'last_name': review.user.last_name,
+        }
+        all_reviews.append(this_review)
+    average_rating = average_rating / len(all_reviews)
+
+    return render_template('album_view.html', album_data = album_data, all_reviews = all_reviews, average_rating = average_rating)
 
 # TODO View Artist
