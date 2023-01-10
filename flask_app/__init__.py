6378f17f85655f94657385603907e5fd0f43f7ee
emilio-fv <emiliofv.sd@gmail.com>
2023-01-10 14:06:30 -0700

    Comments

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask # Import Flask class
+
+app = Flask(__name__) # Instantiate WSGI application 
+
 app.secret_key = 'askdjf;lkaj'
 app.config['SESSION_COOKIE_NAME'] = "Note-d's Cookie"
 DATABASE = 'noted_db'
