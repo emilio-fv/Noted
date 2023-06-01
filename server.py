b893b4e4b1a012b4c2e770ec65195d751bf36431
emilio-fv <emiliofv.sd@gmail.com>
2023-05-31 23:45:39 -0700

    sprint 5 set up

--- server.py
+++
@@ -1,7 +0,0 @@
-# Resource Imports
-from flask_app import app
-from flask_app.controllers import connections_controller, helpers, users_controller, reviews_controller, music_controller
-
-# Run App
-if __name__=='__main__':
-    app.run(debug=True)
