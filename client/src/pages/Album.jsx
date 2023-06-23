86549505bac0c472c6c5f344b151ac57d22d4c8e
emilio-fv <emiliofv.sd@gmail.com>
2023-06-22 17:25:21 -0700

    refactor reviews feature

--- client/src/pages/Album.jsx
+++ client/src/pages/Album.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MainLayout from '../layouts/Main';
 
-import AlbumProfile from '../components/Profile/AlbumProfile';
+import AlbumProfile from '../components/Profiles/AlbumProfile';
 
 const Album = () => {
   return (
--- client/src/pages/Artist.jsx
+++ client/src/pages/Artist.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import MainLayout from '../layouts/Main';
-import ArtistProfile from '../components/Profile/ArtistProfile';
+import ArtistProfile from '../components/Profiles/ArtistProfile';
 
 const Artist = () => {
   return (
