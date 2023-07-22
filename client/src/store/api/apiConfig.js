edfb060be5af62bc3a7870351d3bbc1be400ed3c
emilio-fv <emiliofv.sd@gmail.com>
2023-07-21 23:07:05 -0700

    vercel client setup

--- client/src/store/api/apiConfig.js
+++ client/src/store/api/apiConfig.js
@@ -2,7 +2,8 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export const baseUrl = isProduction ? "https://noted-server.vercel.app/" : 'http://localhost:8000';
+// export const baseUrl = isProduction ? "https://noted-server.vercel.app/" : 'http://localhost:8000';
+export const baseUrl = "https://noted-server.vercel.app/"
 
 // Auth Base Query
 export const authBaseQuery = fetchBaseQuery({
